37022f80fed0037f9b6ffe2d0454179b4926603a
aliciamartin22 <42975338+aliciamartin22@users.noreply.github.com>
2018-09-26 12:31:16 -0400

    updated redirect page

--- profile-processing.php
+++ profile-processing.php
@@ -1,19 +1,16 @@
 <?php
 
-    header("Location: confirmation.php");
+    $uploaddir = 'uploads/';
+    $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
+    if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+        header("Location: confirmation.php");
+    } else {
+        echo "Possible file upload attack!\n";
+    }
 
+    echo 'Here is some more debugging info:';
+    print_r($_FILES);
 
-    // $uploaddir = 'uploads/';
-    // $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-
-    // if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    //     echo "File is valid, and was successfully uploaded.\n";
-    // } else {
-    //     echo "Possible file upload attack!\n";
-    // }
-
-    // echo 'Here is some more debugging info:';
-    // print_r($_FILES);
 
 ?>
